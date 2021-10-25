a9cff7b0bfc9701dc23616899caacf8287f1b037
sergioatk <sergioatk@gmail.com>
2021-10-25 12:01:51 -0300

    adds addGif function

---
+++ src/services/getGifs.js
@@ -0,0 +1,22 @@
+export default async function getGifs(key, term) {
+
+    const apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${key}&q=${term}&limit=10`;
+
+
+    const call = await fetch(apiURL);
+    const json = await call.json();
+
+    const { data } = json;
+    const gifs = data.map(gif => (
+        {
+            title: gif.title,
+            url: gif.images.downsized_medium.url
+        }
+
+    ));
+    
+    return gifs;
+
+}
+
+
